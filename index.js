b991e746af29fb89fee32fda7461da198405775d
Zahran Kabir <zahran@Zahrans-MacBook-Pro.local>
2018-03-12 15:56:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+const driver = {'name': "sam"}
+
+function updateDriverWithKeyAndValue(driver, key, value){
+  return Object.assign({}, driver, {[key]: value})
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+  driver[key] = value
+  return driver
+}
+
+function deleteFromDriverByKey(driver, key){
+  driver_clone = Object.assign({}, driver)
+  delete driver_clone[key]
+  return driver_clone
+
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+  delete driver[key]
+  return driver
+}
